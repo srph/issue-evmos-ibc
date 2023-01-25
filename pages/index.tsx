dfc7105b0cb1492dd093019c4ba941922a9d2b31
Kier Borromeo <seraphipod@gmail.com>
2023-01-25 14:13:20 +0800

    Offline signer not working

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,11 @@ import BigNumber from "bignumber.js";
 import { useChain } from "@cosmos-kit/react";
 import { WalletSection } from "../components";
 import { useMutation } from "react-query";
+import {
+  createMessageSend,
+  createTxIBCMsgTransfer,
+} from "@tharsis/transactions";
+import { decodePubkey } from "@cosmjs/proto-signing";
 
 const TIMEOUT_TIMESTAMP = 5 * 60 * 1000 * 1000 * 1000;
 
@@ -45,24 +50,95 @@ export default function Home() {
 
     console.log(fee);
 
-    const msgTransfer = {
-      typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
-      value: {
-        sourcePort: "transfer",
-        sourceChannel: "channel-25",
-        token: {
-          amount: String(convertDenomToMicroDenom(IBC_AMOUNT, 1e18)),
-          denom: "aevmos",
-        },
-        sender: evmos.address,
-        receiver: stride.address,
-        timeoutTimestamp: generateIbcTimeoutTimestamp(),
-      },
-    };
+    // const msgTransfer = {
+    //   typeUrl: "/ibc.applications.transfer.v1.MsgTransfer",
+    //   value: {
+    //     sourcePort: "transfer",
+    //     sourceChannel: "channel-25",
+    //     token: {
+    //       amount: String(convertDenomToMicroDenom(IBC_AMOUNT, 1e18)),
+    //       denom: "aevmos",
+    //     },
+    //     sender: evmos.address,
+    //     receiver: stride.address,
+    //     timeoutTimestamp: generateIbcTimeoutTimestamp(),
+    //   },
+    // };
 
     const client = await evmos.getSigningStargateClient();
 
-    await client.signAndBroadcast(evmos.address, [msgTransfer], fee, "");
+    console.log("Client", client);
+
+    const { accountNumber, sequence } = await client.getSequence(evmos.address);
+
+    const offlineSigner = await evmos.getOfflineSigner();
+
+    console.log("Offline signer", offlineSigner);
+
+    const accounts = await offlineSigner.getAccounts();
+
+    const account = accounts.find((account) => {
+      return account.address === evmos.address;
+    });
+
+    if (!account) {
+      throw new Error("Account could not be found");
+    }
+
+    const message = createTxIBCMsgTransfer(
+      {
+        chainId: 9001,
+        cosmosChainId: evmos.chain.chain_id,
+      },
+      {
+        accountAddress: evmos.address,
+        sequence,
+        accountNumber,
+        pubkey: account.pubkey.toString(),
+      },
+      {
+        amount: "0",
+        denom: "aevmos",
+        gas: "200000",
+      },
+      "",
+      {
+        sourcePort: "transfer",
+        sourceChannel: "channel-25",
+        amount: String(convertDenomToMicroDenom(IBC_AMOUNT, 1e18)),
+        denom: "aevmos",
+        receiver: stride.address,
+        revisionNumber: 0,
+        revisionHeight: 0,
+        timeoutTimestamp: generateIbcTimeoutTimestamp().toString(),
+      }
+    );
+
+    console.log("message", message);
+
+    await window.keplr.signDirect(
+      evmos.chain.chain_id,
+      evmos.address,
+      message.signDirect
+    );
+
+    // const message = createMessageSend({
+    //   chainId: 9001,
+    //   cosmosChainId: evmos.chain.chain_id
+    // }, {
+    //   accountAddress: evmos.address,
+    //   sequence,
+    //   accountNumber,
+    //   pubkey: account.pubkey
+    // }, {
+    //   amount: '0',
+    //   denom :'aevmos',
+    //   gas: '200000'
+    // }, '', {
+    //   destinationAddress: ''
+    // })
+
+    // await client.signAndBroadcast(evmos.address, [msgTransfer], fee, "");
   };
 
   const handleSuccess = () => {
